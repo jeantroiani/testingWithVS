e6d15b43d078d0d6062b9b6bf04b3b4c6b45f4ee
Jean Troiani <digitalguest@gmail.com>
2015-09-22 09:48:14 +0100

    Calculator class passes when testing 0 and one number

---
+++ StringCalculator_Tests/GivenAStringCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GivenAStringCalculator
+{
+    [TestFixture]
+    public class WhenAddIsCalled_WithEmptyString
+    {
+        [Test]
+        public void ThenZeroIsReturned()
+        {
+            StringCalculator stringCalculator = new StringCalculator();
+            int actual = stringCalculator.Add(string.Empty);
+
+            Assert.That(actual, Is.EqualTo(0));
+        }
+    }
+
+    [TestFixture]
+    public class WhenAddIsCalled_WithOneNumber
+    {
+        [Test]
+        [TestCase("1", 1)]
+        [TestCase("10", 9)]
+        public void ThenTheArgumentIsReturned(string stringToConvert, int expected)
+        {
+            StringCalculator stringCalculator = new StringCalculator();
+            int actual = stringCalculator.Add(stringToConvert);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
